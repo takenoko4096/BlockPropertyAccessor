b3500382a6801c39641b7c4ee28b127cf6d88f8d
たけのこII <yu2.aoki0229.@gmail.com>
2026-02-12 18:54:40 +0900

    r

---
+++ src/main/java/com/gmail/takenokoii78/blockpropertyaccessor/BlockBinarySearchLayerizer.java
@@ -0,0 +1,190 @@
+package com.gmail.takenokoii78.blockpropertyaccessor;
+
+import com.gmail.takenokoii78.json.JSONFile;
+import com.gmail.takenokoii78.json.values.JSONArray;
+import com.gmail.takenokoii78.json.values.JSONObject;
+import net.minecraft.world.level.block.state.properties.Property;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.BlockType;
+import org.bukkit.craftbukkit.block.CraftBlockType;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class BlockBinarySearchLayerizer {
+    public static final String IDENTIFIER = "id";
+
+    public static final String PROPERTIES = "properties";
+
+    private final List<BlockType> list;
+
+    public BlockBinarySearchLayerizer(List<BlockType> list) {
+        this.list = list;
+    }
+
+    public void layerize() {
+        layerize(list.size(), BlockPropertyAccessor.FUNCTION_DIRECTORY);
+
+        final Path entrypoint = BlockPropertyAccessor.FUNCTION_DIRECTORY.resolve(".mcfunction");
+        try {
+            final List<String> lines = new ArrayList<>(Files.readAllLines(entrypoint));
+            lines.addFirst(String.format(
+                "data remove storage %s: %s",
+                BlockPropertyAccessor.NAMESPACE,
+                IDENTIFIER
+            ));
+            lines.addFirst(String.format(
+                "data remove storage %s: %s",
+                BlockPropertyAccessor.NAMESPACE,
+                PROPERTIES
+            ));
+            Files.write(entrypoint, lines, StandardOpenOption.TRUNCATE_EXISTING);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<BlockType> layerize(int size, Path directory) {
+        try {
+            Files.createDirectories(directory);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (size <= 2) {
+            final List<BlockType> values = new ArrayList<>();
+            if (!list.isEmpty()) values.add(list.removeFirst());
+            if (!list.isEmpty()) values.add(list.removeFirst());
+
+            final Path path = directory.resolve(".mcfunction");
+            final List<String> blockForkerLines = new ArrayList<>();
+
+            for (final BlockType blockType : values) {
+                final NamespacedKey key = blockType.getKey();
+                final Path specificBlockFunctionPath = directory.resolve(key.value() + ".mcfunction");
+                final String functionName = BlockPropertyAccessor.FUNCTION_DIRECTORY
+                    .relativize(directory.resolve(key.value()))
+                    .toString()
+                    .replaceAll("\\\\", "/");
+
+                blockForkerLines.add(String.format(
+                    "execute if block ~ ~ ~ %s run function %s:%s",
+                    key,
+                    BlockPropertyAccessor.NAMESPACE,
+                    functionName
+                ));
+
+                final List<String> finalLines = new ArrayList<>();
+                finalLines.add(String.format(
+                    "data modify storage %s: %s set value \"%s\"",
+                    BlockPropertyAccessor.NAMESPACE,
+                    IDENTIFIER,
+                    key
+                ));
+
+                final Collection<Property<?>> properties = ((CraftBlockType<?>) blockType).getHandle()
+                    .defaultBlockState().getProperties();
+
+                for (final Property<?> property : properties) {
+                    property.getAllValues().forEach(value -> {
+                        finalLines.add(String.format(
+                            "execute if block ~ ~ ~ #%s:%s[%s=%s] run data modify storage %s: %s set value %s",
+                            BlockPropertyAccessor.NAMESPACE,
+                            property.getName(),
+                            value,
+                            BlockPropertyAccessor.NAMESPACE,
+                            PROPERTIES,
+                            property.getName(),
+                            value
+                        ));
+                    });
+                }
+
+                try {
+                    Files.createFile(specificBlockFunctionPath);
+                    Files.write(specificBlockFunctionPath, finalLines);
+                }
+                catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            try {
+                Files.createFile(path);
+                Files.write(path, blockForkerLines, StandardOpenOption.TRUNCATE_EXISTING);
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return values;
+        }
+        else {
+            final List<BlockType> values0 = layerize(size / 2, directory.resolve("0"));
+            final List<BlockType> values1 = layerize(size / 2, directory.resolve("1"));
+
+            final Path relative = BlockPropertyAccessor.FUNCTION_DIRECTORY.relativize(directory);
+            final Path tagDirectory = BlockPropertyAccessor.TAGS_BLOCK_DIRECTORY.resolve(relative);
+
+            try {
+                Files.createDirectories(tagDirectory);
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            blockTag(tagDirectory.resolve("0.json"), values0);
+            blockTag(tagDirectory.resolve("1.json"), values1);
+
+            final Path functionPath = directory.resolve(".mcfunction");
+            try {
+                Files.createFile(functionPath);
+                Files.write(functionPath, List.of(
+                    String.format(
+                        "execute if block ~ ~ ~ #%s:%s run function %s:%s",
+                        BlockPropertyAccessor.NAMESPACE,
+                        relative.resolve("0").toString().replaceAll("\\\\", "/"),
+                        BlockPropertyAccessor.NAMESPACE,
+                        relative.resolve("0").toString().replaceAll("\\\\", "/") + '/'
+                    ),
+                    String.format(
+                        "execute if block ~ ~ ~ #%s:%s run function %s:%s",
+                        BlockPropertyAccessor.NAMESPACE,
+                        relative.resolve("1").toString().replaceAll("\\\\", "/"),
+                        BlockPropertyAccessor.NAMESPACE,
+                        relative.resolve("1").toString().replaceAll("\\\\", "/") + '/'
+                    )
+                ));
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            final List<BlockType> values = new ArrayList<>();
+            values.addAll(values0);
+            values.addAll(values1);
+            return values;
+        }
+    }
+
+    private void blockTag(Path path, List<BlockType> values) {
+        final JSONObject object = new JSONObject();
+        object.set("replace", false);
+        final JSONArray array = new JSONArray();
+        for (final BlockType value : values) {
+            array.add(value.getKey().toString());
+        }
+        object.set("values", array);
+
+        final JSONFile file = new JSONFile(path);
+        if (!file.exists()) file.create();
+        file.write(object);
+    }
+}
--- src/main/java/com/gmail/takenokoii78/blockpropertyaccessor/BlockPropertyAccessor.java
+++ src/main/java/com/gmail/takenokoii78/blockpropertyaccessor/BlockPropertyAccessor.java
@@ -1,54 +1,33 @@
 package com.gmail.takenokoii78.blockpropertyaccessor;
 
 import com.gmail.subnokoii78.gpcore.files.ResourceAccess;
-import com.gmail.takenokoii78.json.JSONFile;
-import com.gmail.takenokoii78.json.values.JSONArray;
-import com.gmail.takenokoii78.json.values.JSONObject;
 import io.papermc.paper.registry.RegistryAccess;
 import io.papermc.paper.registry.RegistryKey;
 import net.kyori.adventure.text.Component;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.Property;
 import org.bukkit.Bukkit;
-import org.bukkit.Registry;
 import org.bukkit.block.BlockType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jspecify.annotations.NullMarked;
 
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Field;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @NullMarked
 public final class BlockPropertyAccessor extends JavaPlugin {
-    private static final String DATAPACK_ROOT_DIRECTORY_PATH = "BlockPropertyAccessor" + '-' + Bukkit.getMinecraftVersion();
+    public static final String BLOCK_PROPERTY_ACCESSOR = "BlockPropertyAccessor";
 
-    private static final String DATAPACK_NAMESPACE = "block_property_accessor";
+    public static final String NAMESPACE = "block_property_accessor";
 
-    private static final String ALL_BLOCKS_TAG_NAME = "all";
+    public static final Path ROOT_DIRECTORY = Path.of(BLOCK_PROPERTY_ACCESSOR + '-' + Bukkit.getMinecraftVersion());
 
-    private static final String BLOCK_ID_FINDER_FUNCTION_NAME = "get_block_id";
+    public static final Path DATA_DIRECTORY = ROOT_DIRECTORY.resolve("data");
 
-    private static final String BLOCK_STATES_COLLECTOR_FUNCTION_NAME = "get_block_states";
+    public static final Path NAMESPACE_DIRECTORY = DATA_DIRECTORY.resolve(NAMESPACE);
 
-    private static final String STORAGE_PATH_OUT = "out";
+    public static final Path FUNCTION_DIRECTORY = NAMESPACE_DIRECTORY.resolve("function");
 
-    private static final String STORAGE_PATH_IDENTIFIER = STORAGE_PATH_OUT + '.' + "id";
-
-    private static final String STORAGE_PATH_PROPERTIES = STORAGE_PATH_OUT + '.' + "properties";
-
-    private static final int SEPARATOR = 34;
-
-    private static final String DATAPACK_NAMESPACE_DIRECTORY_PATH = DATAPACK_ROOT_DIRECTORY_PATH + "/data/" + DATAPACK_NAMESPACE;
-
-    private static final String DATAPACK_BLOCK_TAGS_DIRECTORY_PATH = DATAPACK_NAMESPACE_DIRECTORY_PATH + "/tags/block";
-
-    private static final String DATAPACK_FUNCTION_TAGS_DIRECTORY_PATH = DATAPACK_NAMESPACE_DIRECTORY_PATH + "/tags/function";
-
-    private static final String DATAPACK_FUNCTION_DIRECTORY_PATH = DATAPACK_NAMESPACE_DIRECTORY_PATH + "/function";
+    public static final Path TAGS_BLOCK_DIRECTORY = NAMESPACE_DIRECTORY.resolve("tags/block");
 
     @Override
     public void onLoad() {
@@ -59,172 +38,20 @@ public final class BlockPropertyAccessor extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getComponentLogger().info("BlockPropertyAccessor が起動しました");
-        create();
-        getComponentLogger().info("データパック {} が生成されました", DATAPACK_ROOT_DIRECTORY_PATH);
+
+        final ResourceAccess resourceAccess = new ResourceAccess(BLOCK_PROPERTY_ACCESSOR);
+        resourceAccess.copy(ROOT_DIRECTORY);
+
+        final List<BlockType> blockTypes = RegistryAccess.registryAccess().getRegistry(RegistryKey.BLOCK).stream().toList();
+        final BlockBinarySearchLayerizer layerizer = new BlockBinarySearchLayerizer(new ArrayList<>(blockTypes));
+        layerizer.layerize();
+
+        getComponentLogger().info("データパック {} が生成されました", ROOT_DIRECTORY);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getComponentLogger().info(Component.text("BlockPropertyAccessor が停止しました"));
-    }
-
-    private void allBlocksTagJson(JSONFile file) {
-        final JSONArray values = new JSONArray();
-
-        final Registry<BlockType> registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.BLOCK);
-
-        for (final BlockType blockType : registry) {
-            values.add(blockType.getKey().toString());
-        }
-
-        final JSONObject root = new JSONObject();
-        root.set("replace", false);
-        root.set("values", values);
-
-        if (!file.exists()) file.create();
-        file.write(root);
-    }
-
-    private void statesCollectorMcfunction(File file) {
-        final List<String> lines = new ArrayList<>();
-        lines.add(String.format(
-            "data modify storage %s: %s set value {}",
-            DATAPACK_NAMESPACE,
-            STORAGE_PATH_PROPERTIES
-        ));
-
-        for (final Field field : BlockStateProperties.class.getDeclaredFields()) {
-            final Object obj;
-            try {
-                obj = field.get(null);
-            }
-            catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-
-            if (!(obj instanceof Property<?> property)) {
-                continue;
-            }
-
-            property.getAllValues().forEach(value -> lines.add(String.format(
-                "execute if block ~ ~ ~ #%s:%s[%s=%s] run data modify storage %s: %s.%s set value %s",
-                DATAPACK_NAMESPACE,
-                ALL_BLOCKS_TAG_NAME,
-                property.getName(),
-                value.value(),
-                DATAPACK_NAMESPACE,
-                STORAGE_PATH_PROPERTIES,
-                property.getName(),
-                value.value()
-            )));
-        }
-
-        try {
-            if (!file.exists()) Files.createFile(file.toPath());
-            Files.write(file.toPath(), lines, StandardOpenOption.TRUNCATE_EXISTING);
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void identifierFinderMcfunctions(File directory) {
-        if (!directory.isDirectory()) {
-            throw new RuntimeException("not a directory");
-        }
-
-        final Registry<BlockType> registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.BLOCK);
-
-        int i = 1;
-        JSONArray array = new JSONArray();
-        List<String> lines = new ArrayList<>();
-
-        for (final BlockType blockType : registry) {
-            array.add(blockType.getKey().toString());
-            lines.add(String.format(
-                "execute if block ~ ~ ~ %s run data modify storage %s: %s set value %s",
-                blockType.getKey(),
-                DATAPACK_NAMESPACE,
-                STORAGE_PATH_IDENTIFIER,
-                blockType.getKey()
-            ));
-
-            if (i >= SEPARATOR) {
-                final JSONObject tag = new JSONObject();
-                tag.set("replace", false);
-                tag.set("values", array);
-
-                final JSONFile tagFile = new JSONFile(DATAPACK_BLOCK_TAGS_DIRECTORY_PATH + '/' + i + ".json");
-                if (!tagFile.exists()) tagFile.create();
-                tagFile.write(tag);
-
-                final Path functionPath = Path.of(DATAPACK_FUNCTION_DIRECTORY_PATH + '/' + i + ".mcfunction");
-                try {
-                    if (!Files.exists(functionPath)) Files.createFile(functionPath);
-                    Files.write(functionPath, lines, StandardOpenOption.TRUNCATE_EXISTING);
-                }
-                catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-                array = new JSONArray();
-                lines = new ArrayList<>();
-            }
-
-            i++;
-        }
-
-        final Path path = Path.of(DATAPACK_FUNCTION_DIRECTORY_PATH + '/' + BLOCK_ID_FINDER_FUNCTION_NAME);
-        final List<String> idFuncLines = new ArrayList<>();
-
-        idFuncLines.add(String.format(
-            "data modify storage %s: %s set value minecraft:air",
-            DATAPACK_NAMESPACE,
-            STORAGE_PATH_IDENTIFIER
-        ));
-
-        for (int j = 1; j < i; j++) {
-            idFuncLines.add(String.format(
-                "execute if block ~ ~ ~ #%s:%d run function %s:%s",
-                DATAPACK_NAMESPACE,
-                j,
-                DATAPACK_NAMESPACE,
-                j
-            ));
-        }
-
-        try {
-            if (!Files.exists(path)) Files.createFile(path);
-            Files.write(path, idFuncLines, StandardOpenOption.TRUNCATE_EXISTING);
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void create() {
-        final ResourceAccess resourceAccess = new ResourceAccess(DATAPACK_ROOT_DIRECTORY_PATH);
-        resourceAccess.copy(Path.of(DATAPACK_ROOT_DIRECTORY_PATH));
-
-        final JSONFile tagJson = new JSONFile(Path.of(
-            DATAPACK_BLOCK_TAGS_DIRECTORY_PATH + '/' + ALL_BLOCKS_TAG_NAME + ".json")
-        );
-        allBlocksTagJson(tagJson);
-
-        identifierFinderMcfunctions(new File(DATAPACK_FUNCTION_DIRECTORY_PATH));
-        statesCollectorMcfunction(new File(DATAPACK_FUNCTION_DIRECTORY_PATH + '/' + BLOCK_STATES_COLLECTOR_FUNCTION_NAME + ".mcfunction"));
-
-        final JSONFile fTagJson = new JSONFile(Path.of(
-            DATAPACK_FUNCTION_TAGS_DIRECTORY_PATH + '/' + ".json"
-        ));
-        final JSONObject obj = new JSONObject();
-        final JSONArray values = new JSONArray();
-        values.add(DATAPACK_NAMESPACE + ':' + BLOCK_ID_FINDER_FUNCTION_NAME);
-        values.add(DATAPACK_NAMESPACE + ':' + BLOCK_STATES_COLLECTOR_FUNCTION_NAME);
-        obj.set("replace", false);
-        obj.set("values", values);
-        if (!fTagJson.exists()) fTagJson.create();
-        fTagJson.write(obj);
     }
 }
